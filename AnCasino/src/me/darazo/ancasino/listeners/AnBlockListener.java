8c3bcf1c9a931d10ed83d9ab0b97372f8600bb92
GoalieGuy6 <l.mikelonis@yahoo.com>
2012-02-01 08:11:15 -0500

    Update for bukkit 1.1-R3

--- AnCasino/src/me/darazo/ancasino/listeners/AnBlockListener.java
+++ AnCasino/src/me/darazo/ancasino/listeners/AnBlockListener.java
@@ -4,10 +4,12 @@ import me.darazo.ancasino.AnCasino;
 import me.darazo.ancasino.slot.SlotMachine;
 
 import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockListener;
 
-public class AnBlockListener extends BlockListener {
+public class AnBlockListener implements Listener {
 	
 	protected AnCasino plugin;
 	
@@ -15,6 +17,7 @@ public class AnBlockListener extends BlockListener {
 		this.plugin = plugin;
 	}
 	
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onBlockBreak(BlockBreakEvent event) {
 		
 		// Check if plugin is enabled
